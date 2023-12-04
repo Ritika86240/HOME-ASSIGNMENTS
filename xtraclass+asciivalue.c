fab131895dccf7ccf8ccae098adfc2e6341a6845
Ritika86240 <146440022+Ritika86240@users.noreply.github.com>
2023-12-04 23:35:09 +0530

    Add files via upload

---
+++ xtraclass+asciivalue.c
@@ -0,0 +1,46 @@
+/*#include<stdio.h>
+void main ()
+{
+printf("im learning  c programming");
+
+}
+
+#include<stdio.h>
+void main ()
+{
+    int a;
+    printf(" enter the number ");
+    scanf(" %d",&a);
+    (a<10)?printf(" less than 10"):printf(" greater");
+    
+}
+  #include<stdio.h>
+ void main ()
+ {
+int a;
+printf(" enter the number");
+scanf(" %d",&a);
+(10<a<100)? printf(" i'm intrested in  c programming"):printf(" i'm not intrested in c programming");
+ }
+#include<stdio.h>
+void main ()
+{
+  int a;
+  printf( "enter the number:");
+  scanf(" %d",&a);
+  (a==10||a==100)? printf("1"): printf("2");
+    
+}
+ #include <stdio.h>
+void main () 
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    (a!=100)?printf("1"):printf("0");
+}*/
+
+// ascii american standard code for information interchange
+//A=65 TO Z=90
+//a=97 to z=122
+//0=48 to 9=57
