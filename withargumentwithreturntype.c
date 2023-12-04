4d49ca54ca368ba4e139a926e05d1af3446920f8
Ritika86240 <146440022+Ritika86240@users.noreply.github.com>
2023-12-04 23:31:56 +0530

    Add files via upload

---
+++ withargumentwithreturntype.c
@@ -0,0 +1,18 @@
+//functions programs 
+// with argument and return type
+#include<stdio.h>
+void main ()
+{
+    int add(int,int);
+    int a,b,c;
+    printf("enter the two numbers ");
+    scanf(" %d%d",&a,&b);
+    c=add(a,b);
+    printf("sum=%d",c);
+
+} 
+int add(int x,int y)
+{
+    int z;
+z=x+y; return z;
+}
